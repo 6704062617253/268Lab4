032be131c7ecf27153d7b649a9a281c0f43fa2a2
6704062617253-Itsarapong <s6704062617253@email.kmutnb.ac.th>
2026-01-05 16:01:16 +0700

    lab4no9.c N grade version 2

---
+++ lab4no9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(void) {
+    int score;
+    
+    while(1) {
+        scanf("%d", &score);
+        
+        if(score == -1) {
+            break;
+        }
+        
+        if(score >= 68) {
+            if(score > 100) {
+                printf("error score\n");
+            } else if(score >= 85) {
+                printf("%d(A)\n", score);
+            } else if(score >= 75) {
+                printf("%d(B)\n", score);
+            } else {
+                printf("%d(C)\n", score);
+            }
+        } else {
+            if(score >= 55) {
+                printf("%d(D)\n", score);
+            } else if(score < 0) {
+                printf("error score\n");
+            } else {
+                printf("%d(F)\n", score);
+            }
+        }
+    }
+    
+    return 0;
+}
